57d36fe8cb8ddbb30313d0a7c2ea07aae32a1dc4
EgorYakovin <aaa>
2022-11-29 00:16:58 +0300

    work with logic

--- RandomPersonFromApiApp/ViewControllers/ViewController.swift
+++ RandomPersonFromApiApp/ViewControllers/ViewController.swift
@@ -11,16 +11,24 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var getPersonButton: UIButton!
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet var personInfoLabels: [UILabel]!
+    
     @IBOutlet weak var imageView: UIImageView!
     
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var genderLabel: UILabel!
-    var personn: Person!
-    
+    var n = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        activityIndicator.isHidden = true
+        personInfoLabels.forEach { label in
+            label.text = " "
+        }
+        activityIndicator.hidesWhenStopped = true
     }
     
     private func getPerson() {
@@ -32,7 +40,6 @@ class ViewController: UIViewController {
             }
             do {
                 let person = try JSONDecoder().decode(Person.self, from: data)
-                print(person)
                 DispatchQueue.main.async {
                     self.updateUI(with: person)
                     self.updateImage(with: person)
@@ -44,16 +51,24 @@ class ViewController: UIViewController {
     }
     
     @IBAction func getPersonButtonPressed() {
-        if getPersonButton.currentTitle == "Get random person" {
+        switch getPersonButton.currentTitle {
+        case "Get random person":
             getPersonButton.setTitle("Get another person", for: .normal)
+        default:
+            imageView.isHidden = true
+            activityIndicator.startAnimating()
         }
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
         getPerson()
     }
-    private func updateUI(with person: Person) {
+    
+        private func updateUI(with person: Person) {
         self.genderLabel.text = person.results[0].gender
         self.emailLabel.text = person.results[0].email
         self.nameLabel.text = "\(person.results[0].name.first) \(person.results[0].name.last)"
         self.locationLabel.text = "\(person.results[0].location.city), \(person.results[0].location.country)"
+        activityIndicator.stopAnimating()
     }
     
     private func updateImage(with person: Person) {
@@ -65,6 +80,7 @@ class ViewController: UIViewController {
             print(response)
             guard let image = UIImage(data: data) else { return }
             DispatchQueue.main.async {
+                self.imageView.isHidden = false
                 self.imageView.image = image
             }
         }.resume()
